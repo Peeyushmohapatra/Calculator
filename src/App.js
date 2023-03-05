efb0964c5550a9fd66d01eedb2dfdb244ec0c1a4
Peeyushmohapatra <peeyushm1998@gmail.com>
2023-03-05 14:35:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Button from "./Button";
+import Display from "./Display";
 
 function App() {
+  const [val, setVal] = useState("");
+  const [display, updateDisplay] = useState("");
+  const [trial, change] = useState(false);
+  function updateVal(valu) {
+    setVal(val + valu);
+    console.log("Value");
+  }
+
+  function evalInput() {
+    updateDisplay(eval(val));
+    setVal(eval(val));
+  }
+
+  function erase() {
+    setVal("");
+    updateDisplay("");
+    console.log("erase");
+  }
+
+  useEffect(() => {
+    updateDisplay(val);
+  }, [val]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Display display={display} />
+        <Button updateVal={updateVal} evalInput={evalInput} erase={erase} />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// function Calc(e){
+//   setInput(input+e);
+// }
+// function getResult(){
+//   setdisplay(eval(input))
+// }
+// function resetsfunc(){
+//   setdisplay("")
+//   setInput("")
+// }
+// useEffect(()=>{
+//   setdisplay(input)
+
+// },[input])
+
+// Calc={Calc} getResult={getResult} resetsfunc={resetsfunc}
